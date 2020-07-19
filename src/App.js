d6427371a1d6e3aaab99781410a40c260ffb603b
Peter Huang <peterhonline@gmail.com>
2020-07-19 16:26:34 -0400

    Semi working tree diagram

--- src/App.js
+++ src/App.js
@@ -2,10 +2,31 @@ import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./frontend/css/main.css";
 import * as d3 from "d3";
+import { tree } from "d3";
 
 const GAME_DATA =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
 
+const COLORS = [
+  "#caf0f8",
+  "#ddd8c4",
+  "#a3c9a8",
+  "#84b59f",
+  "#69a297",
+  "#50808e",
+  "#ff9f1c",
+  "#ffbf69",
+  "#cbf3f0",
+  "#2ec4b6",
+  "#ff99c8",
+  "#fcf6bd",
+  "#fcf6bd",
+  "#d0f4de",
+  "#a9def9",
+  "#4cc9f0",
+  "#3c6e71",
+  "#74c69d",
+];
 function App() {
   const [data, setData] = useState([]);
 
@@ -20,7 +41,7 @@ function App() {
       if (req.readyState === 4 && req.status === 200) {
         const data = JSON.parse(req.responseText);
 
-        console.log(data);
+        setData(data);
       }
     };
     req.send();
@@ -47,6 +68,84 @@ function App() {
 }
 
 function TreeMap({ data }) {
+  useEffect(() => {
+    if (data.children != null) {
+      drawTreeMap(data);
+    }
+  }, [data]);
+
+  const drawTreeMap = (data) => {
+    const padding = {
+      top: 25,
+      right: 25,
+      bottom: 25,
+      left: 25,
+    };
+
+    const treePadding = 2;
+
+    const dim = {
+      width: 700 + padding.right + padding.left,
+      height: 500 + padding.top + padding.bottom,
+    };
+
+    // Tooltip
+    const tooltip = d3
+      .select("#body")
+      .append("div")
+      .attr("id", "tooltip")
+      .attr("style", "position: absolute; opacity: 0;");
+
+    // Graph Titles
+    d3.select("#treemap")
+      .append("div")
+      .attr("id", "title")
+      .text("Video Game Sales");
+
+    d3.select("#title")
+      .append("div")
+      .attr("id", "description")
+      .text("Top 100 Most Sold Video Games Grouped by Platform");
+
+    const treemap = d3.treemap();
+    treemap.size([dim.width, dim.height]).paddingOuter(treePadding);
+
+    const root = d3.hierarchy(data);
+
+    //console.log(data[1].children);
+    root.sum((d) => {
+      return d.value;
+    });
+
+    treemap(root);
+
+    // SVG setup
+    const svg = d3
+      .select("#treemap")
+      .append("svg")
+      .attr("width", dim.width)
+      .attr("height", dim.height);
+
+    svg
+      .selectAll("rect")
+      .data(root.leaves())
+      .enter()
+      .append("rect")
+      .attr("fill", (d, i) => {
+        //console.log(d.parent.data.name);
+        return "blue";
+      })
+      .attr("x", (d) => d.x0)
+      .attr("y", (d) => d.y0)
+      .attr("width", (d) => d.x1 - d.x0)
+      .attr("height", (d) => d.y1 - d.y0)
+      .attr("data-name", (d) => {
+        console.log(d.parent.children);
+      })
+      .attr("data-category", "")
+      .attr("data-value", "");
+  };
+
   return (
     <div id="treemap-container">
       <div id="treemap"></div>
